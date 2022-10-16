bbaec6eb0750410d3641eca616e1ff72cfa2df76
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-16 18:17:00 -0300

    Criado o teste de integração para listar todas as pessoas

--- tests/integration/people.test.js
+++ tests/integration/people.test.js
@@ -3,11 +3,12 @@ const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const app = require('../../src/app');
 const connection = require('../../src/db/connection');
-const { person } = require('../mocks/peopleMocks');
+const { person, peopleList } = require('../mocks/peopleMocks');
 
 const { use, expect } = chai;
 
 use(chaiHttp);
+
 
 describe('Testando os endpoints da entidade `people`', function () {
     it('Testando o cadastro de uma pessoa ', async function () {
@@ -21,6 +22,23 @@ describe('Testando os endpoints da entidade `people`', function () {
         expect(response.status).to.deep.equal(201);
         expect(response.body).to.deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
     });
+
+    it('Testando a listagem de todas as pessoas', async function () {
+        sinon.stub(connection, 'execute').resolves([peopleList]);
+
+        console.log(await connection.execute()); // Retorna o array do mock
+
+        const response = await chai
+        .request(app)
+        .get('/people');
+
+        console.log(response); // Imprime o objeto response no console
+
+        expect(response.status).to.deep.equal(200);
+        expect(response.body).to.deep.equal(peopleList);
+    });
+
+    it('Testando a listagem da pessoa com id 1', async function () {});
 
     afterEach(sinon.restore);
 });
--- tests/mocks/peopleMocks.js
+++ tests/mocks/peopleMocks.js
@@ -5,6 +5,24 @@ const person = {
     phone: '851 678 4453',
 }
 
+const peopleList = [
+    {
+      id: 1,
+      firstName: 'Luke',
+      lastName: 'Skywalker',
+      email: 'luke.skywalker@trybe.com',
+      phone: '851 678 4453',
+    },
+    {
+      id: 2,
+      firstName: 'Dart',
+      lastName: 'Vader',
+      email: 'dart.vader@trybe.com',
+      phone: '851 678 5665',
+    },
+  ];
+
 module.exports = {
     person,
+    peopleList,
 };
