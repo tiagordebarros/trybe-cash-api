0265861a183fe9ad3bd4f7864fb83ba7e49de15f
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-17 15:09:45 -0300

    Definidas as variáveis de ambiente e criado o arquivo de exemplo

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
+require('dotenv').config();
 const app = require("./app");
 const connection = require("./db/connection");
 
-const port = 3001;
+const port = process.env.PORT;
 
 app.listen(port, async () => {
-  console.log(`Bem vindo, ${process.env.USER}.`);
-  console.log(`API TrybeCash está sendo executada na porta ${port}.`);
+  console.log(`Bem vindo, ${process.env.USER_NAME}.`);
+  console.log(`TrybeCash API está sendo executado na porta ${port}.`);
 
-  const [result] = await connection.execute(
-    'SELECT * FROM trybecashdb.people'
-  );
+  const [result] = await connection.execute('SELECT 1');
   
   if (result) return console.log('Conexão com o banco de dados estabelecida com sucesso!');
 });
