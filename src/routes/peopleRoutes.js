f331790894b363baa7ed15f64f9a97b4eef9e1e4
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-17 14:43:35 -0300

    Endpoints refatorados para tratar dos casos de erro

--- src/routes/peopleRoutes.js
+++ src/routes/peopleRoutes.js
@@ -16,26 +16,45 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (_req, res) => {
     const result = await findAll();
-    return res.status(200).json(result);
+    try {
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({ message: err.sqlMessage });
+    }
 });
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
-    const [result] = await findById(id);
-    return res.status(200).json(result);
+    try {
+        const [result] = await findById(id);
+        if (result) return res.status(200).json(result);
+        return res.status(404).json({ message: 'Pessoa não encontrada' });
+    } catch (err) {
+        return res.status(500).json({ message: err.sqlMessage });
+    }
 });
 
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
     const person = req.body;
     const [result] = await update(person, id);
-    if (result.affectedRows > 0) return res.status(200).json({ message: `Pessoa de id ${id} atualizada com sucesso` });
+    try {
+        if (result.affectedRows > 0) return res.status(200).json({ message: `Pessoa de id ${id} atualizada com sucesso` });
+        return res.status(404).json({ message: 'Pessoa não encontrada' });
+    } catch (error) {
+        return res.status(500).json({ message: err.sqlMessage });
+    }
 });
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
     const [result] = await remove(id);
-    if (result.affectedRows > 0) return res.status(200).json({ message: `Pessoa de id ${id} excluída com sucesso` });
+    try {
+        if (result.affectedRows > 0) return res.status(200).json({ message: `Pessoa de id ${id} excluída com sucesso` });
+        return res.status(404).json({ message: 'Pessoa não encontrada' });
+    } catch (error) {
+        return res.status(500).json({ message: err.sqlMessage });
+    }
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,15 +4,12 @@ const connection = require("./db/connection");
 const port = 3001;
 
 app.listen(port, async () => {
+  console.log(`Bem vindo, ${process.env.USER}.`);
   console.log(`API TrybeCash está sendo executada na porta ${port}.`);
 
   const [result] = await connection.execute(
     'SELECT * FROM trybecashdb.people'
   );
-
-  if (result) {
-    console.log('Conexão MYSQL estabelecida com sucesso!');
-  } else {
-    console.log('Falha na conexão ao banco de dados MYSQL');
-  }
+  
+  if (result) return console.log('Conexão com o banco de dados estabelecida com sucesso!');
 });
